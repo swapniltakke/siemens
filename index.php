a820e0c4d759e7d9c4a1cfa75829fde4738580f2
swapniltakke <108459592+swapniltakke@users.noreply.github.com>
2024-08-04 18:23:36 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Website</title>
+    <title>Swapnil Takke</title>
 </head>
 <body>
     <header>
-        <h1>Welcome to My Website</h1>
+        <h1>Hello and welcome to Swapnil Takke's personal website!</h1>
     </header>
     <main>
         <?php include 'create_user.html'; ?>
